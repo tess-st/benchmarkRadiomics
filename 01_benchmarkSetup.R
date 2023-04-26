ec7c071ea70add312ce378fce119e250823d7b4f
tess-st <Teessii@gmx.de>
2023-04-26 10:22:13 +0200

    added benchmark setup

---
+++ 01_benchmarkSetup.R
@@ -0,0 +1,180 @@
+options("mlr3.debug" = TRUE)
+# load different data sets
+load("../data/saved_data/final_data_clinic.RData")
+load("../data/saved_data/final_data_omics.RData")
+load("../data/saved_data/final_data_cd_omics.RData")
+
+# Load datasets
+# only radiomics datsets from liver (rd_l)and tumor /rd-t) provided, clinical data  (cd) and combinations not available
+TASK_IDS = c("rd_l", "rd_t")# , "cd", "cd_rd_l", "cd_rd_t")
+
+# Define measure(s) for tuning and evaluation
+# "surv.graf" = integrated Brier-score (IBS)
+TUNING_MEASURE = "surv.graf"
+SCORE_MEASURES = c("surv.graf", "surv.cindex") # c-index actually not needed
+
+# Learners
+# provide the name of all learners with settings as names in a vector
+LEARNER_IDS = c(
+  "surv.cv_glmnet-tuned",
+  "surv.cv_glmnet-untuned",
+  "surv.cv_glmnet-pca-tuned",
+  "surv.cv_glmnet-pca-untuned",
+  "surv.rfsrc-tuned",
+  "surv.rfsrc-untuned",
+  "surv.rfsrc-pca-tuned",
+  "surv.rfsrc-pca-untuned",
+  "surv.xgboost-tuned",
+  "surv.xgboost-untuned",
+  "surv.xgboost-pca-tuned",
+  "surv.xgboost-pca-untuned"
+)
+
+# set some default hyperparamter settings
+LEARNER_PAR_VALUES = list(
+  "surv.cv_glmnet" = list(parallel = TRUE),
+  "surv.rfsrc" = list(ntree = 500L, na.action = "na.impute"),
+  "surv.xgboost" = list(nrounds = 500L, verbose = 0)
+)
+
+# define a fallback learner, such that benchmark does not throw an error and stops the benchmark experiment
+FALLBACK_LEARNER_ID = "surv.kaplan"
+
+# Pipeline
+getGraphLearner = function(learner_id, learner) {
+  # PCA
+  no_pca = po("nop", "no_pca")
+  # pipe for pca, keep principal components explaining 90% of the variance (filter.frac)
+  pca = po("pca", scale. = TRUE) %>>% po("filter",
+    filter = mlr3filters::flt("variance", task_type = "surv"), filter.frac = 0.9)
+  # define the two options pca vs. no pca
+  pca_opts = list(PCA = pca, PCA_None = no_pca)
+  # set up branch with pca options
+  pca_branch = po("branch", id = "pca_branch",
+    options = names(pca_opts)) %>>%
+    gunion(pca_opts) %>>%
+    po("unbranch", id = "pca_unbranch", options = names(pca_opts))
+
+  # feature selection filters
+  # correlation based
+  fs_corr = po("filter", id = "corr_filter", mlr3filters::flt("find_correlation", task_type = "surv"),
+    affect_columns = selector_type(c("numeric", "integer")))
+  # based on feature importance of a rf
+  fs_rfimp = po("filter", id = "rfimp_filter", mlr3filters::flt("importance", learner = lrn("surv.ranger",
+    importance = "permutation")))
+  # based on information gain
+  fs_ig = po("filter", id = "ig_filter", mlr3filters::flt("information_gain", equal = TRUE, task_type = "surv"))
+  # list all feature selection methods
+  fs_opts = list(FS_Corr = fs_corr, FS_RFimp = fs_rfimp, FS_IG = fs_ig, FS_None = po("nop", "no_fs"))
+  # define the feature selection branch
+  fs_branch = po("branch", id = "fs_branch", options = names(fs_opts)) %>>%
+    gunion(fs_opts) %>>%
+    po("unbranch", id = "fs_unbranch", options = names(fs_opts))
+
+  # combine the different pipelines in a graph models
+  if (learner_id == "surv.cv_glmnet" || learner_id == "surv.xgboost") {
+    return(
+      as_learner(
+        po("imputemode") %>>%
+          po("encode") %>>%
+          po("imputehist") %>>%
+          po("removeconstants") %>>%
+          po("fixfactors") %>>%
+          fs_branch %>>%
+          pca_branch %>>%
+          po("copy", outnum = 2) %>>%
+          gunion(list(po("learner", lrn("surv.kaplan")), learner)) %>>%
+          po("compose_distr") # separate calculation of distr for glmnet and xgboost to calculate surv.graf -> need for if statement
+      )
+    )
+  } else {
+    return(
+      as_learner(
+        po("imputeoor") %>>%
+          po("encode") %>>%
+          po("imputehist") %>>%
+          po("removeconstants") %>>%
+          po("fixfactors") %>>%
+          fs_branch %>>%
+          pca_branch %>>%
+          po("learner", learner) # returns distr anyway
+      )
+    )
+  }
+}
+
+# Resampling for evaluation, 10-fold cross validation
+RESAMPLING_OUTER = rsmp("cv", folds = 10L)
+
+# Resampling for tuning, 5-fold cross validation
+RESAMPLING_INNER = rsmp("cv", folds = 5L)
+
+# Settings for tuners
+ETA = 2
+N_EVALS = 200L
+
+# tuner: for xgboost and rfsrc hyperband, in case of glmnet no meaningful eta definable -> use random search instead
+getTuner = function(learner_id) {
+  if (learner_id == "surv.cv_glmnet") {
+    return(tnr("random_search"))
+  }
+  else {
+    return(tnr("hyperband", eta = ETA))
+  }
+}
+
+# set a terminator after N_EVALS rounds for cv_glmnet
+getTerminator = function(learner_id) {
+  if (learner_id == "surv.cv_glmnet") {
+    return(trm("evals", n_evals = N_EVALS))
+  }
+  else {
+    return(trm("none"))
+  }
+}
+
+# function for extracting all tuning parameters depending on each learner
+getTuningParams = function(learner_id, task) {
+  if (learner_id == "surv.cv_glmnet") {
+    search_space = ps(
+      surv.cv_glmnet.alpha = p_dbl(lower = 0, upper = 1),
+      fs_branch.selection = p_fct(levels = c("FS_Corr", "FS_RFimp", "FS_IG", "FS_None")), # necessary for tuning over fs methods
+      FS_Corr.corr_filter.filter.cutoff = p_dbl(0.1, 0.9),
+      FS_RFimp.rfimp_filter.filter.frac = p_dbl(0.1, 0.9),
+      FS_IG.ig_filter.filter.frac = p_dbl(0.1, 0.9)
+    )
+  } else if (learner_id == "surv.rfsrc") {
+    search_space = ps(
+      surv.rfsrc.ntree = p_int(lower = 20L, upper = 1500L, tags = "budget"),
+      surv.rfsrc.mtry = p_int(lower = 3L, upper = ceiling((task$ncol)^(1/1.5))),
+      surv.rfsrc.nodesize = p_int(lower = 1L, upper = task$nrow),
+      fs_branch.selection = p_fct(levels = c("FS_Corr", "FS_RFimp", "FS_IG", "FS_None")),
+      FS_Corr.corr_filter.filter.cutoff = p_dbl(0.1, 0.9),
+      FS_RFimp.rfimp_filter.filter.frac = p_dbl(0.1, 0.9),
+      FS_IG.ig_filter.filter.frac = p_dbl(0.1, 0.9)
+    )
+  } else if (learner_id == "surv.xgboost") {
+    search_space = ps(
+      surv.xgboost.nrounds = p_int(lower = 20L, upper = 1500L, tags = "budget"),
+      surv.xgboost.eta = p_dbl(lower = -4L, upper = 0, trafo = function(x) 10^x),
+      surv.xgboost.max_depth = p_int(lower = 1L, upper = 20),
+      surv.xgboost.colsample_bylevel = p_dbl(lower = 0.1, upper = 1),
+      surv.xgboost.lambda = p_int(lower = -10, upper = 10, trafo = function(x) 2^x),
+      surv.xgboost.alpha = p_int(lower = -10, upper = 10, trafo = function(x) 2^x),
+      surv.xgboost.subsample = p_dbl(lower = 0.1, upper = 1),
+      fs_branch.selection = p_fct(levels = c("FS_Corr", "FS_RFimp", "FS_IG", "FS_None")),
+      FS_Corr.corr_filter.filter.cutoff = p_dbl(0.1, 0.9),
+      FS_RFimp.rfimp_filter.filter.frac = p_dbl(0.1, 0.9),
+      FS_IG.ig_filter.filter.frac = p_dbl(0.1, 0.9)#,
+    )
+  }
+
+  search_space$add_dep("FS_Corr.corr_filter.filter.cutoff",
+    "fs_branch.selection", CondEqual$new("FS_Corr")) # necessary for tuning over fs methods
+  search_space$add_dep("FS_RFimp.rfimp_filter.filter.frac",
+    "fs_branch.selection", CondEqual$new("FS_RFimp"))
+  search_space$add_dep("FS_IG.ig_filter.filter.frac",
+    "fs_branch.selection", CondEqual$new("FS_IG"))
+
+  return(search_space)
+}
---
+++ 02_batchmark.R
@@ -0,0 +1,125 @@
+# load predefined data sets and functions
+source("0_2_benchmarkSetup.R", local = TRUE)
+
+# function to extract each task (varying data sets)
+getTask = function(task_id) {
+  # pick each data set based on task_id and remove column "pat_id"
+  # event variable needs to be numeric instead of factor for survival
+  data = get(task_id) %>%
+    dplyr::select(-c("pat_id")) %>%
+    mutate(death = (as.numeric(death) - 1))
+
+  # clinical data: remove all binary variables created from a numeric
+  # if(grepl("cd", task_id))
+  #   data = data %>%
+  #     dplyr::select(!(names(cd)[grep("bin", names(cd))]))
+
+  # define task
+  task = TaskSurv$new(
+    id = paste("sirflox", task_id, sep = "-"),
+    backend = data,
+    time    = "osdays",
+    event   = "death"
+  )
+
+  # stratum
+  task$col_roles$stratum = "death"
+  return(task)
+}
+
+# function to extract specific graph learner set up, mainly from the defined names in learner_id
+getLearner = function(learner_id, task) {
+  fallback_learner = lrn(FALLBACK_LEARNER_ID)
+  # split the name in learner_id according to "-" in multiple parts, defining each pipeline
+  id = stringr::str_split(learner_id, "-", simplify = TRUE)[1, ]
+  PCA = ifelse("pca" %in% id, "PCA", "PCA_None")
+  TUNING = ifelse("tuned" %in% id, TRUE, FALSE)
+  # the learner name is the first character in the vector
+  learner_id = id[1]
+  # the rsf provides the prediction type distribution anyway, which is necessary for calculating the IBS
+  # for the other two learners one needs a work around
+  predict_type = ifelse(learner_id == "surv.rfsrc", "distr", "crank")
+  learner = lrn(learner_id, predict_type = predict_type)
+  # extract and set the predefined hyperparameter values according to the id
+  learner$param_set$values = LEARNER_PAR_VALUES[[learner_id]]
+  # learner$fallback = fallback_learner
+
+  # linear graph learner
+  glearner = getGraphLearner(learner_id, learner)
+  glearner$param_set$values$pca_branch.selection = PCA
+
+  # if the pipeline does not include the tuning step, only "no feature selection" is allowed in the preprocessing part
+  if (!TUNING)
+    glearner$param_set$values$fs_branch.selection = "FS_None"
+  # glearner$encapsulate = ENC
+
+  glearner$id = paste(learner_id, PCA, sep = "_")
+
+  # tuning
+  if (TUNING) {
+    search_space = getTuningParams(learner_id, task)
+
+    # autotuner for hyperparameters, search space and tuner/terminator depending on learner
+    at = AutoTuner$new(
+      learner = glearner,
+      resampling = RESAMPLING_INNER,
+      measure = msr(TUNING_MEASURE, integrated = TRUE, times = seq(0, quantile(cd$osdays, .80))),
+      search_space = search_space,
+      tuner = getTuner(learner_id),
+      terminator = getTerminator(learner_id)#,
+      # store_tuning_instance = TRUE,
+      # store_benchmark_result = TRUE,
+      # store_models = TRUE
+    )
+    # at$encapsulate = ENC
+    return (at)
+  } else {
+    # no tuning
+    return(glearner)
+  }
+}
+
+# list of tasks
+tasks = lapply(TASK_IDS, getTask)
+learners = unlist(lapply(tasks, function(t) lapply(LEARNER_IDS, function(l) {
+  learner = getLearner(l, t)
+  learner$id = paste(learner$id, t$id, sep = "_")
+  return(learner)})))
+
+# resampling with instantiation
+set.seed(29102021)
+resamplings = RESAMPLING_OUTER$clone(deep = TRUE)$instantiate(tasks[[1]])
+
+# design of the benchmark/batchmark
+design = data.table(
+  task = rep(tasks, each = length(LEARNER_IDS)),
+  learner = learners,
+  resampling = rep(list(resamplings), length(learners))
+)
+
+# # setup for batchmark (use this on cluster instead of benchmark)
+# unlink("radiomics-bmr-filters", recursive = TRUE)
+#
+# reg = batchtools::makeExperimentRegistry(
+#   file.dir = "radiomics-bmr-filters",
+#   packages = PACKAGES,
+#   source = c("3_2_1_benchmarkSetup_filters.R", "server_settings.R"),
+#   seed = 123
+# )
+#
+# # registry
+# reg = loadRegistry("radiomics-bmr-filters", writeable = TRUE)
+# reg$default.resources = list(
+#   walltime = 3600L * 48L,
+#   memory = 1024L * 48L,
+#   ntasks = 1L,
+#   ncpus = 56L,
+#   nodes = 1L,
+#   clusters = "cm2_tiny",
+#   partition = "cm2_tiny"
+# )
+
+# batchmark(design, reg = reg)#, store_models = TRUE)
+# getStatus()
+
+bench = benchmark(design)#, store_models = TRUE)
